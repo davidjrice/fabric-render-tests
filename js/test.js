c63180cca4a3c1767193875daeb74b30c0b4886a
David Rice <me@davidjrice.co.uk>
2012-08-15 13:02:12 +0100

    More prototyping, try webkit as an svg rendering engine

--- js/test.js
+++ js/test.js
@@ -5,6 +5,44 @@ describe('ImageTest', function() {
     this.addMatchers(imagediff.jasmine);
   });
 
+  it('should convert be the same image', function () {
+
+    var svg_url = '/spec/svg/10.svg';
+    var canvas = null,
+        data   = null;
+
+    var
+      a = new Image(),
+      b = new Image();
+
+    fabric.loadSVGFromURL(svg_url, function(objects, options){
+      var shape = fabric.util.groupSVGElements(objects, options);
+      canvas = new fabric.Canvas('canvas-10');
+      canvas.setWidth(shape.width);
+      canvas.setHeight(shape.height);
+
+      canvas.add(shape).centerObjectH(shape).centerObjectV(shape).renderAll();
+      shape.setCoords();
+      canvas.calcOffset();
+      data = canvas.toDataURL('png');
+
+      a.src = data;
+      b.src = 'images/inkscape.10a.png';
+
+    })
+    
+
+    waitsFor(function (){
+      return a.complete & b.complete;
+    }, 'image not loaded.', 2000);
+
+    runs(function () {
+      console.log(a)
+      expect(a).toImageDiffEqual(b);
+    });
+  });
+
+
   // Test
   it('should convert be the same image', function () {
 
@@ -20,6 +58,24 @@ describe('ImageTest', function() {
 
     runs(function () {
       expect(a).toImageDiffEqual(b);
+    });
+  });
+
+
+  it('should convert be the same image', function () {
+
+    var
+      a = new Image(),
+      b = new Image();
+    a.src = 'images/fabric.1.png';
+    b.src = 'images/illustrator.1.png';
+
+    waitsFor(function () {
+      return a.complete & b.complete;
+    }, 'image not loaded.', 2000);
+
+    runs(function () {
+      expect(a).toImageDiffEqual(b, 1000);
     });
   });
 
@@ -47,6 +103,23 @@ describe('ImageTest', function() {
       b = new Image();
     a.src = 'images/fabric.69.png';
     b.src = 'images/inkscape.69a.png';
+
+    waitsFor(function () {
+      return a.complete & b.complete;
+    }, 'image not loaded.', 2000);
+
+    runs(function () {
+      expect(a).toImageDiffEqual(b);
+    });
+  });
+
+  it('should convert be the same image', function () {
+
+    var
+      a = new Image(),
+      b = new Image();
+    a.src = 'images/fabric.69.png';
+    b.src = 'images/webkit.69.png';
 
     waitsFor(function () {
       return a.complete & b.complete;
